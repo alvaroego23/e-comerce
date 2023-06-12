7931592dacbe99f42bd8c48d7a5c5463ff8624a0
Alvaroego <alvaroego@uci.cu>
2023-06-12 13:12:09 -0400

    add part2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ResultadoBusquedaComponent } from './components/resultado-busqueda/resultado-busqueda.component';
+import { PaginaProductoComponent } from './components/pagina-producto/pagina-producto.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ResultadoBusquedaComponent,
+    PaginaProductoComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/pagina-producto/pagina-producto.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pagina-producto',
+  templateUrl: './pagina-producto.component.html',
+  styleUrls: ['./pagina-producto.component.scss']
+})
+export class PaginaProductoComponent {
+
+}
