64d49c42842d8e0e02c34a1c5d94480024fbd27e
Alvaroego <alvaroego@uci.cu>
2023-06-13 18:06:23 -0400

    pagina  product view

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PaginaProductoComponent } from './components/pagina-producto/pagina-producto.component';
+import { ResultadoBusquedaComponent } from './components/resultado-busqueda/resultado-busqueda.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'productos', component:PaginaProductoComponent},
+  {path:'', component:ResultadoBusquedaComponent},
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/components/resultado-busqueda/resultado-busqueda.component.ts
+++ src/app/components/resultado-busqueda/resultado-busqueda.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-resultado-busqueda',
   templateUrl: './resultado-busqueda.component.html',
   styleUrls: ['./resultado-busqueda.component.scss']
 })
-export class ResultadoBusquedaComponent {
+export class ResultadoBusquedaComponent  implements OnInit{
+  
+  constructor(private router:Router){}
+
+  ngOnInit(): void {
+    
+  }
+
+  products(){
+    this.router.navigate(['productos']);
+  }
 
 }
